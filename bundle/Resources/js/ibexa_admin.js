2ecb4f3cadebd8e95489e580660180903163230a
Vesna <vesna@netgen.io>
2023-09-20 12:12:59 +0200

    IOTA-313 configure ibexa admin for tinymce

--- bundle/Resources/js/ibexa_admin.js
+++ bundle/Resources/js/ibexa_admin.js
@@ -1,66 +1,25 @@
-// // import moj_ckeditor from '../public/ckeditor/build/ckeditor';
-//
-// // import CKEditor from '@ckeditor/ckeditor5-editor-inline/src/inlineeditor';
-//
-// const ready = (callback) => {
-//     if (document.readyState !== "loading") callback();
-//     else document.addEventListener("DOMContentLoaded", callback);
-// }
-//
-// ready(async () => {
-//     console.log(CKEditor);
-//     //
-//     // console.log("window.ibexa.richText.CKEditor");
-//     // console.log(typeof window.ibexa.BaseRichText !== 'undefined');
-//     //
-//     if (typeof window.ibexa.BaseRichText !== 'undefined') {
-//         console.log("u ifu");
-//         CKEditor = require('@ckeditor/ckeditor5-editor-inline/src/inlineeditor');
-//     }
-//     const ngHtmlTextEditFields = document.querySelectorAll('.ibexa-field-edit--nghtmltext');
-//
-//     ngHtmlTextEditFields.forEach((ngHtmlTextEditField) => {
-//
-//         const ckEditorTextArea = ngHtmlTextEditField.querySelector('textarea');
-//
-//         // CKEditor.init(ckEditorTextArea);
-//         console.log(ckEditorTextArea);
-//         // CKEDITOR.create(ckEditorTextArea).catch( error => {
-//         //     console.error( error );
-//         // } );
-//         // CKEDITOR.create(ckEditor).catch( error => {
-//         //     console.error( error );
-//         // } );
-//
-//         // CKEDITOR.editor(ckEditorTextArea);
-//
-//     });
-//
-//
-//
-//     // const ckEditorTextareas = Array.from(document.querySelectorAll('.ibexa-field-edit--nghtmltext > textarea'));
-//
-//     // ckEditorTextareas.forEach((ckEditor) => {
-//     //     CKEDITOR.create(ckEditor).catch( error => {
-//     //         console.error( error );
-//     //     } );
-//     // });
-// });
-//
-//
-// // import Editor from '../public/ckeditor/build/ckeditor';
-// //
-// // const ready = (callback) => {
-// //     if (document.readyState !== "loading") callback();
-// //     else document.addEventListener("DOMContentLoaded", callback);
-// // }
-// //
-// // ready(async () => {
-// //     const ckEditorTextareas = Array.from(document.querySelectorAll('.nghtmltext > textarea'));
-// //
-// //     ckEditorTextareas.forEach((ckEditor) => {
-// //         Editor.create(ckEditor).catch( error => {
-// //             console.error( error );
-// //         } );
-// //     });
-// // });
+import tinymce from '../public/tinymce/js/tinymce/tinymce.min'
+import '../public/tinymce/js/tinymce/themes/silver/theme.min';
+import '../public/tinymce/js/tinymce/plugins/link/plugin';
+
+const ready = (callback) => {
+    if (document.readyState !== "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+
+ready(() => {
+
+    console.log(document.querySelectorAll('.ibexa-field-edit--nghtmltext textarea'));
+
+    tinymce.init({
+        selector: '.ibexa-field-edit--nghtmltext textarea',
+        base_url: '/bundles/netgenibexafieldtypehtmltext/tinymce/js/tinymce',
+        statusbar: false,
+        menubar: false,
+        toolbar: 'undo redo | blocks | ' +
+            'bold italic underline strikethrough | alignleft aligncenter ' +
+            'alignright alignjustify | blockquote | bullist numlist | outdent indent | ' +
+            'link unlink',
+        plugins: 'link'
+    });
+});
